15c6012d12ecf8906817ca1ca7b94304c2f504ec
Cristhian Heck <cristhian.heck@ifc.edu.br>
2022-03-18 08:58:54 -0300

    Ajustes de código

--- lib/services/remote.dart
+++ lib/services/remote.dart
@@ -2,25 +2,25 @@ import 'package:pokemarvel/models/pokemon.dart';
 import 'package:pokemarvel/models/pokemonList.dart';
 import 'package:http/http.dart' as http;
 
-class RemoteService{
+class RemoteService {
   static Future<PokemonList>? getPokemonsList() async {
-    
     var client = http.Client();
-    var uri = Uri.parse('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151');
+    var uri = Uri.parse('https://pokeapi.co/api/v2/pokemon/');
     var response = await client.get(uri);
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       var json = response.body;
       return pokemonListFromJson(json);
     } else {
-      return PokemonList(results: new List.empty());
-    }  
+      return PokemonList(results: List.empty());
+    }
   }
 
   static Future<Pokemon>? getPokemon(var number) async {
     var client = http.Client();
-    var uri = Uri.parse('https://pokeapi.co/api/v2/pokemon/'+number.toString());
+    var uri =
+        Uri.parse('https://pokeapi.co/api/v2/pokemon/' + number.toString());
     var response = await client.get(uri);
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       var json = response.body;
       return pokemonFromJson(json);
     } else {
